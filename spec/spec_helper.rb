7a51e7d27c7900e729bf74668d2c9da145057fde
JoaoPierre <jppierre90@gmail.com>
2026-05-22 19:35:13 -0300

    add resource specs and shared test helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require "asaas"
+require "webmock/rspec"
+
+ASAAS_BASE_URL = "https://sandbox.asaas.com/api/v3"
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
@@ -10,4 +13,32 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.before do
+    Asaas.configure do |c|
+      c.api_key     = "aact_test_fake"
+      c.sandbox     = true
+      c.max_retries = 0
+    end
+  end
+end
+
+def stub_asaas(method, path, body: {}, status: 200)
+  stub_request(method, "#{ASAAS_BASE_URL}#{path}")
+    .to_return(
+      status: status,
+      body: body.to_json,
+      headers: { "Content-Type" => "application/json" }
+    )
+end
+
+def list_response(items, has_more: false)
+  {
+    "object" => "list",
+    "hasMore" => has_more,
+    "totalCount" => items.size,
+    "limit" => 10,
+    "offset" => 0,
+    "data" => items
+  }
 end
