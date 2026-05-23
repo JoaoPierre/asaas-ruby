a7aa96b3f9e229a0347b767a54bc21bcafbe38ae
JoaoPierre <jppierre90@gmail.com>
2026-05-23 20:57:58 -0300

    Add asaas sandbox + spec

---
+++ lib/asaas/resources/sandbox.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Asaas
+  module Resources
+    class Sandbox < Base
+      def self.resource_path = "/sandbox"
+
+      def self.approve_account
+        response = client.request(:post, "#{resource_path}/myAccount/approve")
+        AsaasObject.construct_from(response)
+      end
+
+      def self.confirm_payment(id)
+        response = client.request(:post, "#{resource_path}/payment/#{id}/confirm")
+        AsaasObject.construct_from(response)
+      end
+
+      def self.overdue_payment(id)
+        response = client.request(:post, "#{resource_path}/payment/#{id}/overdue")
+        AsaasObject.construct_from(response)
+      end
+    end
+  end
+end
